74097c4f91e46fbbdce0d4df03d8342ac2ae7c4f
Marcus <marcus@Marcuss-MacBook-Air.local>
2022-09-27 16:57:12 +0200

    Lade in en class() för spelare, skapde en import for questions.py lade till en variabel i lagra_frågor()

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from questions import questions
+
 def main():
-    def funktion_för_frågor():
+    def funktion_för_frågor():      #Tar emot och skriver ut frågor
         pass
-    funktion_för_frågor()
+       
+    def lagra_frågor():             #Lagrar frågor från questions.py
+        frågor = questions
+        pass
+
+class spelare():                    #Här lagras data om spelare & deras poäng
+    def __init__(self,namn:str,poängtavla:int):
+        assert namn == str(),f"{self.namn} innehåller tecken som inte är bokstäver"
+        
+        self.namn = namn
+        self.poängtavla = poängtavla
+
+
+    def __repr__(self):
+        return f"Namn på spelare: {self.namn}, totalt poäng för {self.namn}: {self.poängtavla} poäng"
+
+
+    #Test av objekt
+Kalle = spelare("Kalle", 5)
+print(Kalle)
+
+    
 
 
 

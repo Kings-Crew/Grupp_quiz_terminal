f24bccc92a4f1f9bbb6c2870b4cfe382c9b07f89
Marcus <marcus@Marcuss-MacBook-Air.local>
2022-09-27 14:56:49 +0200

    Lade upp python filen för alla frågor som skall vara med i quiz programmet

---
+++ questions.py
@@ -0,0 +1,20 @@
+#Alla frågor lagras i en separat fil. Det lagras som en dict() för enkelt åtkomst, 
+#Och för att det på ett enkelt sätt skall kunna skickas som t.ex. en JSON
+
+#Möjligtvis om man skall använda sig av defaultdict biblioteket för multidimensionella dicts()
+#Kanske blir för komplex: https://www.geeksforgeeks.org/defaultdict-in-python/ //Marcus 2022-09-27
+
+questions = [
+    {"Question 1", },
+    {"Question 2", },
+    {"Question 3", },
+    {"Question 4", },
+    {"Question 5", },
+    {"Question 6", },
+    {"Question 7", },
+    {"Question 8", },
+    {"Question 9", },
+    {"Question 10", },
+]
+
+print(questions)

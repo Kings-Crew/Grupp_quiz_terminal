1744e0c7fd81e0bf979c322d8d07942d8da3ca7e
Marcus <marcus@Marcuss-MacBook-Air.local>
2022-09-28 17:11:27 +0200

    Fixade så att spelaren kan avsluta spelet när dem vill, felhantering så att namnet på spelaren inte är en float eller integer

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 from questions import *
+from time import sleep
+poängtavla_ = 0
+namn_ = str()
 #Alla frågor sparas som instansvariabler från klassen: Frågor()
 
-
-class main:
-
-    
-    @staticmethod
-    def funktion_för_frågor():      #Tar emot och skriver ut frågor
-        score = 0
-        for i in range(10):
-            spelar_fråga = input(f"{lista_på_frågor[i]}>>>")      
-
-            str_spelar_fråga = str(spelar_fråga.upper())            #Konverterar till stor bokstav
-
-            if str_spelar_fråga != lista_på_frågor[i].rightanswer:      #Jämför svar
-                print( "Fel svar")
-            elif str_spelar_fråga == lista_på_frågor[i].rightanswer:
-                
-                print("Rätt svar")
-            print(f"Du hade såhär många rätt {score}")
-                
-            
-
-class spelare():                    #Här lagras data om spelare & deras poäng
-    def __init__(self,namn:str,poängtavla:int):
+class Spelare():                    #Här lagras data om spelare & deras poäng
+    def __init__(self,namn:str,poängtavla=0):
         assert type(namn) == str ,"Är inte en string(str) funktion"
         self.namn = namn
         self.poängtavla = poängtavla
@@ -33,9 +15,61 @@ class spelare():                    #Här lagras data om spelare & deras poäng
 
 
 
+class Main:  
+    
+    
+    @staticmethod
+    def funktion_för_frågor():      #Tar emot och skriver ut frågor
+        global poängtavla_
+        
+        for i in range(10):
+            spelar_fråga = input(f"{lista_på_frågor[i]}>>>")      
 
-#Test av objekt
+            str_spelar_fråga = str(spelar_fråga.upper())            #Konverterar till stor bokstav
 
+            if str_spelar_fråga != lista_på_frågor[i].rightanswer:      #Jämför svar
+                print( "Fel svar")
+            elif str_spelar_fråga == lista_på_frågor[i].rightanswer:
+                poängtavla_ += 1
+                print("Rätt svar")
+            
+    
+    
+    def main():                                                     #Detta är funktionen för att spela spelet
+        
+        def felhantering_för_input():            #Felhantering utifall spelaren skriver något konstigt
+            print("Hej och välkommen till King Crew's AB quiz spel")
+            #sleep(1)
+            print("Du ska svara på 10 frågor...")
+            #sleep(1)
+            print("Men först, vad heter du?")
+            global namn_
+            while True:
+                prompt = input(">>>")
+
+                try:
+                    #Konverterar prompt till ett heltal
+                    värde = int(prompt)
+                    print("Du har ett heltal i din input")
+                except ValueError:
+                    try:
+                    #Konverterear prompt till flyttal
+                        val = float(prompt)
+                        print("Input är ett flyttal")
+                    except ValueError:
+                        namn_ = Spelare(prompt,poängtavla_)
+                        break
+        while True:
+            felhantering_för_input()
+            Main.funktion_för_frågor()
+            print(namn_)
+            print("Tryck 1 för JA \nTryck 2 för NEJ")
+            prompt = input("Vill du spela igen? \n>>>")
+            if prompt == "1":
+                continue
+            elif prompt == "2":
+                break
+            
 
 if __name__ == "__main__":
-    print(main.funktion_för_frågor())
+    Main.main()
